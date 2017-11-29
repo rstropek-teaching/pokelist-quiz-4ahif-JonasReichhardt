6e2d5be804fc2468f335337a511e84ed1cd7d0a4
JonasReichhardt <32189954+JonasReichhardt@users.noreply.github.com>
2017-11-29 14:30:38 +0100

    final upload

--- index.js
+++ index.js
@@ -1,70 +1,63 @@
-var pokemonList = document.getElementById('pokemons');
-var detailName = document.getElementById('name');
-var pic = document.getElementById('pic');
-var weight = document.getElementById('weight');
-var moves = document.getElementById('moves');
-var firstPage = 'https://pokeapi.co/api/v2/pokemon/';
 var nextLink;
 var previousLink;
-var link;
-link = firstPage;
+var link = 'https://pokeapi.co/api/v2/pokemon/';
 getPoke();
 function getPoke() {
+    var pokemonList = document.getElementById('pokemons');
     if (pokemonList != null) {
-        (function () {
-            fetch(link).then(function (response) {
-                response.json().then(function (pokelist) {
-                    var html = '';
-                    nextLink = pokelist.next;
-                    previousLink = pokelist.previous;
-                    for (var _i = 0, _a = pokelist.results; _i < _a.length; _i++) {
-                        var pokemon = _a[_i];
-                        html += "<tr><td>" + pokemon.name.toUpperCase() + "</td><td><button onclick=details('" + pokemon.url + "')>Details</button></td></tr>";
-                    }
-                    pokemonList.innerHTML = html;
-                });
+        fetch(link).then(function (response) {
+            response.json().then(function (pokelist) {
+                var html = '';
+                nextLink = pokelist.next;
+                previousLink = pokelist.previous;
+                for (var _i = 0, _a = pokelist.results; _i < _a.length; _i++) {
+                    var pokemon = _a[_i];
+                    html += "<tr><td>" + pokemon.name.toUpperCase() + "</td><td><button class=\"btn btn-primary\"\" onclick=details('" + pokemon.url + "')>Details</button></td></tr>";
+                }
+                pokemonList.innerHTML = html;
             });
-        })();
+        });
     }
 }
 function details(link2) {
+    var detailName = document.getElementById('name');
+    var pic = document.getElementById('pic');
+    var weight = document.getElementById('weight');
+    var moves = document.getElementById('moves');
     if (link2 != null && detailName != null) {
-        (function () {
-            fetch(link2).then(function (response) {
-                response.json().then(function (pokelist2) {
-                    var html = '';
-                    detailName.innerHTML = pokelist2.forms[0].name.toUpperCase();
-                    if (pic != null && weight != null) {
-                        pic.innerHTML = "<img src='" + pokelist2.sprites.front_default + "'>";
-                        weight.innerText = "Weight: " + pokelist2.weight + " lbs";
-                        for (var _i = 0, _a = pokelist2.moves; _i < _a.length; _i++) {
-                            var move = _a[_i];
-                            html += "<li>" + move.move.name.toUpperCase() + "</li>";
-                        }
-                        if (moves != null) {
-                            moves.innerHTML = html;
-                        }
+        fetch(link2).then(function (response) {
+            response.json().then(function (pokelist2) {
+                var html = '';
+                detailName.innerHTML = pokelist2.forms[0].name.toUpperCase();
+                if (pic != null && weight != null) {
+                    pic.innerHTML = "<img src='" + pokelist2.sprites.front_default + "'>";
+                    weight.innerText = "Weight: " + pokelist2.weight + " lbs";
+                    for (var i = 0; i < 4; i++) {
+                        html += "<li>" + pokelist2.moves[i].move.name.toUpperCase() + "</li>";
                     }
-                });
+                    if (moves != null) {
+                        moves.innerHTML = html;
+                    }
+                }
             });
-        })();
+        });
     }
 }
-function next() {
-    if (nextLink != null) {
+function jump(num) {
+    if (nextLink === null) {
+        window.alert("There is no next page!");
+    }
+    else if (num === 0) {
         link = nextLink;
         getPoke();
     }
     else {
-        window.alert("There is no next page!");
-    }
-}
-function previous() {
-    if (previousLink != null) {
-        link = previousLink;
-        getPoke();
-    }
-    else {
-        window.alert("There is no previous page!");
+        if (previousLink === null) {
+            window.alert("There is no previous page!");
+        }
+        else if (num === 1) {
+            link = previousLink;
+            getPoke();
+        }
     }
 }
--- index.ts
+++ index.ts
@@ -1,76 +1,82 @@
-const pokemonList = document.getElementById('pokemons');
-const detailName = document.getElementById('name');
-const pic = document.getElementById('pic');
-const weight = document.getElementById('weight');
-const moves = document.getElementById('moves');
+let nextLink: string;
+let previousLink: string;
 
-let firstPage : string = 'https://pokeapi.co/api/v2/pokemon/';
-let nextLink : string;
-let previousLink : string;
-let link : string;
-
-link = firstPage;
+let link: string = 'https://pokeapi.co/api/v2/pokemon/';
 getPoke();
 
-function getPoke(){
-  if(pokemonList!=null){
-    
-        (function(){
-            fetch(link).then(response => {
-              response.json().then(pokelist => {
-                let html = '';
-                nextLink = pokelist.next;
-                previousLink = pokelist.previous;
-                for (const pokemon of pokelist.results) {
-                  html += `<tr><td>${pokemon.name.toUpperCase()}</td><td><button onclick=details('${pokemon.url}')>Details</button></td></tr>`
-                }
-                pokemonList.innerHTML = html;
-              });
-            });
-            })();
-    }
+
+function getPoke() {
+  const pokemonList = document.getElementById('pokemons');
+
+  if (pokemonList != null) {
+
+    fetch(link).then(response => {
+      response.json().then(pokelist => {
+        let html = '';
+        nextLink = pokelist.next;
+        previousLink = pokelist.previous;
+
+        for (const pokemon of pokelist.results) {
+          html += `<tr><td>${pokemon.name.toUpperCase()}</td><td><button class="btn btn-primary"" onclick=details('${pokemon.url}')>Details</button></td></tr>`
+        }
+
+        pokemonList.innerHTML = html;
+      });
+    });
+  }
 }
 
-function details(link2:string){
-    if(link2!=null&&detailName!=null){
+function details(link2: string) {
+  const detailName = document.getElementById('name');
+  const pic = document.getElementById('pic');
+  const weight = document.getElementById('weight');
+  const moves = document.getElementById('moves');
 
-        (function(){
-          fetch(link2).then(response => {
-            response.json().then(pokelist2 => {
-              let html = '';
-              detailName.innerHTML = pokelist2.forms[0].name.toUpperCase();
-              if(pic!=null&&weight!=null){
-                pic.innerHTML = `<img src='${pokelist2.sprites.front_default}'>`;
-                weight.innerText = "Weight: "+pokelist2.weight +" lbs";
-                for(const move of pokelist2.moves){
-                  html += `<li>${move.move.name.toUpperCase()}</li>`;
-                }
-                if(moves!=null){
-                  moves.innerHTML = html;
-                }
-              }
-            });
-          });
-        })();
+  if (link2 != null && detailName != null) {
 
-    }
+    fetch(link2).then(response => {
+      response.json().then(pokelist2 => {
+        let html = '';
+        detailName.innerHTML = pokelist2.forms[0].name.toUpperCase();
+
+        if (pic != null && weight != null) {
+          pic.innerHTML = `<img src='${pokelist2.sprites.front_default}'>`;
+          weight.innerText = "Weight: " + pokelist2.weight + " lbs";
+
+          for (var i = 0; i < 4; i++) {
+            html += `<li>${pokelist2.moves[i].move.name.toUpperCase()}</li>`;
+          }
+
+          if (moves != null) {
+            moves.innerHTML = html;
+          }
+        }
+
+      });
+    });
+  }
 }
 
-function next(){
-    if(nextLink!=null){
-        link = nextLink;
-        getPoke();
-    }else{
-      window.alert("There is no next page!");
-    }
-}
+function jump(num: number) {
 
-function previous(){
-    if(previousLink!=null){
+  if (nextLink === null) {
+
+    window.alert("There is no next page!");
+
+  } else if (num === 0) {
+
+    link = nextLink;
+    getPoke();
+  } else {
+    if (previousLink === null) {
+
+      window.alert("There is no previous page!");
+
+    } else if (num === 1) {
+
       link = previousLink;
       getPoke();
-    }else{
-      window.alert("There is no previous page!");
     }
+  }
 }
 
